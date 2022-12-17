a8f91d2aae9ef56a5a92b4b155352e834e18075b
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-17 11:59:46 +0700

    create mongo schema

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,7 +8,7 @@ const launch = {
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   target: "Kepler-442 b",
-  customer: ["NASA", "SpaceX"],
+  customers: ["NASA", "SpaceX"],
   upcoming: true,
   success: true,
 };
