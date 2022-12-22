d584ec2121514ef9964fb772c304cc07b0672f23
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-22 20:53:00 +0700

    FUCK ERROR

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,10 +1,12 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "http://localhost:8000/v1";
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches = await response.json();
@@ -13,6 +15,7 @@ async function httpGetLaunches() {
   });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
@@ -23,10 +26,13 @@ async function httpSubmitLaunch(launch) {
       body: JSON.stringify(launch),
     });
   } catch (err) {
-    return { ok: false };
+    return {
+      ok: false,
+    };
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
@@ -34,7 +40,9 @@ async function httpAbortLaunch(id) {
     });
   } catch (err) {
     console.log(err);
-    return { ok: false };
+    return {
+      ok: false,
+    };
   }
 }
 
