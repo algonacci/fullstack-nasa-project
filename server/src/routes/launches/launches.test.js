241881a5adaccc39c8015e4029e529bac4e36cb9
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-28 23:21:40 +0700

    Update launches.test.js

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,10 +2,12 @@ const { response } = require("express");
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../middlewares/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
